f2d642572988c7e0628ab8ca81cef983c6ec639a
JustAPotato0916 <justapotato0916@gmail.com>
2023-10-11 20:55:51 +0800

    chore: update AWS sdk to v3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,43 +1,42 @@
-import AWS from "aws-sdk"
+import { PutObjectCommandOutput, S3 } from "@aws-sdk/client-s3"
 
-export async function uploadToS3(file: File) {
-    try {
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
-        })
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-            },
-            region: "ap-southeast-1"
-        })
+export async function uploadToS3(
+    file: File
+): Promise<{ file_key: string; file_name: string }> {
+    return new Promise((resolve, reject) => {
+        try {
+            const s3 = new S3({
+                region: "ap-southeast-1",
+                credentials: {
+                    accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+                    secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
+                },
+            })
 
-        const file_key = "uploads/" + Date.now().toString() + file.name.replace(" ", "-")
+            const file_key =
+                "uploads/" + Date.now().toString() + file.name.replace(" ", "-")
 
-        const params = {
-            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-            Key: file_key,
-            Body: file
+            const params = {
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+                Key: file_key,
+                Body: file,
+            }
+            s3.putObject(
+                params,
+                (err: any, data: PutObjectCommandOutput | undefined) => {
+                    return resolve({
+                        file_key,
+                        file_name: file.name,
+                    })
+                }
+            )
+        } catch (error) {
+            reject(error)
         }
-
-        const upload = s3.putObject(params).on("httpUploadProgress", (progress) => {
-            console.log("Uploading to S3...", parseInt(((progress.loaded / progress.total) * 100).toString()) + "%")
-        }).promise()
-
-        await upload.then((data) => {
-            console.log("Successfully uploaded to S3!", file_key)
-        })
-
-        return Promise.resolve({
-            file_key,
-            file_name: file.name,
-        })
-
-    } catch (error) { }
+    })
 }
 
 export function getS3Url(file_key: string) {
-    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3-ap-southeast-1.amazonaws.com/${file_key}`
+    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-southeast-1.amazonaws.com/${file_key}`
     return url
 }
