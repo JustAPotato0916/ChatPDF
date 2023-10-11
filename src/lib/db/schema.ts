0c1ca9b0688989ab10dc501a1abdd21c87a33b15
JustAPotato0916 <justapotato0916@gmail.com>
2023-10-11 11:44:34 +0800

    feat: chat sidebar

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -11,6 +11,8 @@ export const chats = pgTable("chats", {
     fileKey: text("file_key").notNull(),
 })
 
+export type DrizzleChat = typeof chats.$inferSelect
+
 export const message = pgTable("message", {
     id: serial("id").primaryKey(),
     chatId: integer("chat_id").references(() => chats.id).notNull(),
